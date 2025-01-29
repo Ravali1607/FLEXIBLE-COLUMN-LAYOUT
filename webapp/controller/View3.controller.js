c43c822708ed8bd08e1491204f123990ff065a30
Ravali Chakka <ravalichakka16@gmail.com>
2025-01-29 09:40:05 +0000

    commit

--- webapp/controller/View3.controller.js
+++ webapp/controller/View3.controller.js
@@ -10,21 +10,23 @@ sap.ui.define([
             this.oEventBus.subscribe("flexible","setView3",this.oBranch,this);
         },
         oBranch: function(sChannel,sEvent,oData){
-            var Branch = oData.branch.EMP_BRANCH;
-            this.getOwnerComponent().getModel().read("/PLANTS",{
-                success: function(response){
-                    var filteredBranchData = response.results.filter(branch => branch.PLANT_LOC === Branch);
-                    console.log(filteredBranchData);
-                    var oModel = new sap.ui.model.json.JSONModel({
-                        plant : filteredBranchData
-                    })
-                    that.byId("plantData").setModel(oModel);
-                    // that.getView().byId("plantData").setModel(oModel);
-                },error: function(error){
-                    sap.m.MessageToast.show("Error");
-                    console.log(error);
-                }
-            })
+            if(oData && oData.branch.EMP_BRANCH){
+                var Branch = oData.branch.EMP_BRANCH;
+                this.getOwnerComponent().getModel().read("/PLANTS",{
+                    success: function(response){
+                        var filteredBranchData = response.results.filter(branch => branch.PLANT_LOC === Branch);
+                        console.log(filteredBranchData);
+                        var oModel = new sap.ui.model.json.JSONModel({
+                            plant : filteredBranchData
+                        })
+                        that.byId("plantData").setModel(oModel);
+                        // that.getView().byId("plantData").setModel(oModel);
+                    },error: function(error){
+                        sap.m.MessageToast.show("Error");
+                        console.log(error);
+                    }
+                })
+            }
         },
         onClose: function(){
             this.oEventBus.publish("flexible","setView2");
